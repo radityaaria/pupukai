523d058ea91814bb68e9c978d8eec64e742acba2
Raditya Aria <ariaramadhani72@gmail.com>
2025-06-19 12:05:08 +0700

    revisi

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -45,31 +45,37 @@ const About = () => {
       </div>
       <div className="mt-10 p-4 bg-gray-100 rounded-lg">
         <h2 className="text-3xl mb-6 font-bold">Penjelasan Objek Penelitian</h2>
-        <p>
+        {/* <p>
           Penelitian ini berfokus pada pengembangan platform digital berupa
-          website sistem pendukung keputusan untuk menentukan kelayakan
-          penerimaan bantuan pupuk. Tujuan utamanya adalah untuk membantu proses
-          seleksi calon penerima bantuan agar lebih objektif, efisien, dan
-          berbasis data, menggunakan metode K-Nearest Neighbor (KNN) sebagai
-          inti dari pengambilan keputusan.
+          website yang membantu penentuan kelayakan penerimaan bantuan pupuk.
+          Tujuan utamanya adalah untuk membantu proses seleksi calon penerima
+          bantuan agar lebih objektif, efisien, dan berbasis data, menggunakan
+          metode K-Nearest Neighbor (KNN) sebagai inti dari penentuan kriteria.
+        </p> */}
+        <p className="text-xl">
+          Penelitian ini berfokus pada pengembangan sebuah platform digital yang
+          bertujuan membantu menentukan kelayakan penerimaan bantuan pupuk.
+          Tujuannya adalah membuat proses seleksi penerima bantuan lebih
+          objektif, efisien, dan berdasarkan data. Selain itu, penelitian ini
+          juga mengkaji bagaimana pemanfaatan kecerdasan buatan dapat
+          meningkatkan transparansi dan akurasi dalam penyaluran bantuan, serta
+          dampaknya terhadap efektivitas program subsidi pertanian. Dengan
+          mengintegrasikan teknologi ini ke dalam proses evaluasi, diharapkan
+          platform ini dapat berkontribusi positif pada pengambilan kebijakan
+          yang lebih adil dan tepat sasaran di sektor pertanian. Objek
+          penelitian ini secara spesifik adalah program penyaluran pupuk organik
+          yang akan dibagikan setiap bulan kepada kelompok tani yang memenuhi
+          kriteria kelayakan. Program ini secara eksklusif ditujukan untuk
+          kelompok tani yang berlokasi di wilayah Bantul.
         </p>
-        <p>
-          Selain itu, penelitian ini juga mengkaji bagaimana pemanfaatan
-          kecerdasan buatan dapat meningkatkan transparansi dan akurasi dalam
-          penyaluran bantuan, serta dampaknya terhadap efektivitas program
-          subsidi pertanian. Dengan mengintegrasikan teknologi AI ke dalam
-          proses evaluasi, diharapkan sistem ini dapat memberikan kontribusi
-          positif bagi pengambilan kebijakan yang lebih adil dan tepat sasaran
-          di sektor pertanian.
-        </p>
-        <p className="mt-4">
+        <p className="mt-4 text-xl">
           Jika Anda memiliki pertanyaan, silakan hubungi kami melalui informasi
           di bawah ini:
         </p>
-        <div className="mt-2">
-          <strong className="text-xl">POLRES Bantul</strong>
+        <div className="mt-4">
+          <strong className="text-2xl">POLRES Bantul</strong>
           <br />
-          <div className="grid grid-cols-[auto,1fr] gap-x-2 max-w-xl">
+          <div className="grid grid-cols-[auto,1fr] gap-x-2 max-w-xl text-xl">
             <span className="font-semibold min-w-max">Alamat:</span>
             <span className="break-words w-full">
               Jl. Jend. Sudirman No.202, Bejen, Bantul, Kec. Bantul, Kabupaten
@@ -82,10 +88,10 @@ const About = () => {
           </div>
         </div>
       </div>
-      <div className="flex justify-center mt-4 gap-4 flex-col md:flex-row items-center">
+      {/* <div className="flex justify-center mt-4 gap-4 flex-col md:flex-row items-center">
         <Image src="/dokumentasi1.jpeg" width={288} height={288} alt="dokumentasi1" style={{objectFit: 'cover'}} className="w-72 h-72 rounded-lg shadow" />
         <Image src="/dokumentasi2.jpeg" width={288} height={288} alt="dokumentasi2" style={{objectFit: 'cover'}} className="w-72 h-72 rounded-lg shadow" />
-      </div>
+      </div> */}
     </div>
   );
 };
